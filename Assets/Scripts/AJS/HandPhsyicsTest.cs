85adaca7d3ca5cd837d849c9ba45f918ad3e43b9
RUTSIMT <rutsimt@gmail.com>
2025-07-03 12:41:32 +0900

    Quaternion 주석
    - Quaternion 개념을 설명한 현학적이고 지루한 주석
    - 물체 A가 물체 B의 위치와 회전을 따라가게 하기 위한 식이다

--- Assets/Scripts/AJS/HandPhsyicsTest.cs
+++ Assets/Scripts/AJS/HandPhsyicsTest.cs
@@ -16,11 +16,16 @@ public class HandPhsyicsTest : MonoBehaviour
     {
         rb.velocity = (target.position - transform.position) / Time.fixedDeltaTime;
 
+        // 타켓과 나의 회전량의 차이를 구하기 위한 Quaternion 계산식 
+        // 타켓의 rotation에 내 역회전량을 곱한다
         Quaternion rotationDifference = target.rotation * Quaternion.Inverse(transform.rotation);
+        // rotationDifference 를 각도와 축 정보로 반환
         rotationDifference.ToAngleAxis(out float angleInDegree, out Vector3 rotationAxis);
 
+        // 해당 정보로 얼만큼 회전해야 하는지 Vector3  형태로 계산
         Vector3 rotationDifferenceInDegree = angleInDegree * rotationAxis;
 
+        // 해당 정보들로, 자연스럽게 돌게 시킴
         rb.angularVelocity = rotationDifferenceInDegree * Mathf.Deg2Rad / Time.fixedDeltaTime;
     }
 }
