8514fce87c2c94d802153a82fba3e74387897a20
RUTSIMT <rutsimt@gmail.com>
2025-07-14 16:07:45 +0900

    애니매이션 수정

--- Assets/Scripts/AJS/HandAnimation.cs
+++ Assets/Scripts/AJS/HandAnimation.cs
@@ -15,6 +15,9 @@ public class HandAnimation : MonoBehaviourPun
     public InputActionReference triggerInput;
     public InputActionReference gripInput;
 
+    private float gripValue = 0f;
+    private float triggerValue = 0f;
+
     private void Update()
     {
 #if LOCAL_TEST
@@ -22,24 +25,23 @@ public class HandAnimation : MonoBehaviourPun
 #elif PHOTON
         bool isLocalPlayer = photonView.IsMine;
 #endif
-        if (!isLocalPlayer) return;
-
-        float triggerValue = 0f;
-        if (triggerInput != null && triggerInput.action != null && triggerInput.action.enabled)
-        {
-            triggerValue = triggerInput.action.ReadValue<float>();
-        }
-
-        float gripValue = 0f;
-        if (gripInput != null && gripInput.action != null && gripInput.action.enabled)
-        {
-            gripValue = gripInput.action.ReadValue<float>();
-        }
 
         if (handAnimator != null)
         {
             handAnimator.SetFloat("Trigger", triggerValue);
             handAnimator.SetFloat("Grip", gripValue);
+        }
+
+        if (!isLocalPlayer) return;
+
+        if (triggerInput != null && triggerInput.action != null && triggerInput.action.enabled)
+        {
+            triggerValue = triggerInput.action.ReadValue<float>();
+        }
+
+        if (gripInput != null && gripInput.action != null && gripInput.action.enabled)
+        {
+            gripValue = gripInput.action.ReadValue<float>();
         }
     }
 }
